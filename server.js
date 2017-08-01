c48325359204d27050bbe709bd8d171efe20bb4a
ktsys <30594332+ktsys@users.noreply.github.com>
2017-08-01 18:18:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*var restify = require('restify');
+var restify = require('restify');
 var builder = require('botbuilder');
 
 // Get secrets from server environment
@@ -25,16 +25,16 @@ server.post('/api/messages', connector.listen());
 
 // Serve a static web page
 
-*/
-//server.get(/.*/, restify.serveStatic({
-/*	'directory': '.',
+
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
 	'default': 'index.html'
 }));
 
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
-*/	
+	
 	
 	
 	
